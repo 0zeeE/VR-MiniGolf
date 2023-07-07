b690abed5cf9039bc2c0c65fee9722d6138c2431
0zeeE <ertekinozgur2000@hotmail.com>
2023-07-07 15:46:18 +0300

    particle devam

--- Assets/Scripts/Concrete/Ozgur/GolfBall.cs
+++ Assets/Scripts/Concrete/Ozgur/GolfBall.cs
@@ -7,8 +7,18 @@ public class GolfBall : MonoBehaviour
     [SerializeField] private AudioSource hitSound;
     [SerializeField] private AudioSource popSound;
     [SerializeField] private float flyAmount = 10f;
+    [SerializeField] private GameObject confettiParticle;
+    [SerializeField] private Vector3 ballPos;
+    [SerializeField] private Quaternion ballQuaternion;
+    
 
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        
+    }
+
     public void PlayHitSound()
     {
         hitSound.Play();
@@ -27,9 +37,18 @@ public class GolfBall : MonoBehaviour
 
     private IEnumerator DestroySequence()
     {
+        //bitmedi
         this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * flyAmount);
         yield return new WaitForSeconds(1);
+        Instantiate(confettiParticle, this.gameObject.transform);
+        //this.gameObject.GetComponent<MeshRenderer>().
+        yield return new WaitForSeconds(1);
+        
+    }
+
+
+    private void AA()
+    {
         Destroy(this.gameObject);
-        //Konfetti kodunu yaz
     }
 }
