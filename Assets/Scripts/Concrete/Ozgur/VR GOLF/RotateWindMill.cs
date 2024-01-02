be48497ead4658b52c0f01bf61b9fb4d415eaf6a
0zeeE <ertekinozgur2000@hotmail.com>
2024-01-03 01:03:11 +0300

    Ufak duzenlemeler

--- Assets/Scripts/Concrete/Ozgur/VR GOLF/RotateWindMill.cs
+++ Assets/Scripts/Concrete/Ozgur/VR GOLF/RotateWindMill.cs
@@ -7,12 +7,7 @@ public class RotateWindMill : MonoBehaviour
     // Start is called before the first frame update
     public GameObject windMill;
     public float rotateSpeed = 1f;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         windMill.transform.Rotate(0, 0, rotateSpeed  * Time.deltaTime);
